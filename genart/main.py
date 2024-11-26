535b0f790be3e6558ba8286844cafc7e7f230a9a
Stuart Olsen <stuart@sj-olsen.com>
2024-11-26 16:15:23 -0600

    Implement image scaling

--- genart/main.py
+++ genart/main.py
@@ -1,24 +1,40 @@
+from typing import Optional
+
 from PySide6 import QtCore
 from PySide6 import QtGui
 from PySide6 import QtWidgets
 import numpy
 
 
+def size_transform(src: QtCore.QSize, dst: QtCore.QSize) -> QtGui.QTransform:
+    """Compute the transformation matrix from one size to another."""
+    sx = dst.width() / src.width()
+    sy = dst.height() / src.height()
+    return QtGui.QTransform().scale(sx, sy)
+
+
 class ImageView(QtWidgets.QWidget):
     """Fixed-sized widget for displaying a dynamically mutated QImage."""
     image: QtGui.QImage
+    _size_hint: QtCore.QSize
+    _paint_transform: QtGui.QTransform
 
-    def __init__(self, image: QtGui.QImage):
+    def __init__(self, image: QtGui.QImage, size: Optional[QtCore.QSize] = None):
         super().__init__()
         self.image = image
-        self.setSizePolicy(QtWidgets.QSizePolicy())
+        self._size_hint = size or image.size()
 
     def sizeHint(self) -> QtCore.QSize:
-        return self.image.size()
+        return self._size_hint
+
+    def resizeEvent(self, event: QtGui.QResizeEvent):
+        self._paint_transform = size_transform(self.size(), self.image.size())
 
     def paintEvent(self, event: QtGui.QPaintEvent):
         with QtGui.QPainter(self) as painter:
-            painter.drawImage(event.rect(), self.image, event.rect())
+            dst = event.rect().toRectF()
+            src = self._paint_transform.mapRect(dst)
+            painter.drawImage(dst, self.image, src)
 
 
 class ImageArray:
@@ -54,10 +70,10 @@ def main(argv: list[str]) -> int:
     # red = x, green = y, blue = checker
     array = ImageArray(image)
     rg = numpy.moveaxis(array.xy, 0, -1)
-    checker = -(numpy.sum(array.xy >> 2, 0) % 2) & 0xff
+    checker = -(numpy.sum(array.xy, 0) % 2) & 0xff
     numpy.copyto(array.rgb, numpy.dstack((rg, checker)), casting='unsafe')
 
-    view = ImageView(image)
+    view = ImageView(image, QtCore.QSize(512, 512))
     window.setCentralWidget(view)
     window.show()
     return app.exec()
